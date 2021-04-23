7c166958926e82ee1ba3e2afbe15262636dcf02e
nyrkuredla <rtkelley@gmail.com>
2021-04-22 21:49:40 -0400

    adding new data set, ex 4 notes, and weekly assignment file

---
+++ exercise3/data-acquisition-assignment01.R
@@ -0,0 +1,9 @@
+# weekly assignment - analyse the sale of houses using the house_listings dataset
+# criteria: runs without error, create 3 ggplot charts, write brief analysis, include given R features
+# features: as.factor, as.integer, as.double, labs, pipe, map data to color, chart includes categorial variable
+
+# first load library
+library (tidyverse)
+
+# explore data structure
+load ("house_listings.RData")
